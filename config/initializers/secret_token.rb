d3faa6f6aa271d69b8da1053dbc36d6615ab105a
davidhiston <david.histon@gmail.com>
2014-08-12 21:27:07 +0100

    Added more key files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mtrottenrotaties::Application.config.secret_token = '2be7a68f86335359adb6ab222afacbbb6dc3507a51cc3838a97dcaf89337673db2f100ae877cd520c60a4e06c10bdc1aa44597bf0b8fded94e5f99ac828b748a'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Mtrottenrotaties::Application.routes.draw do
+  resources :movies
+  root :to => redirect('/movies')
+end
